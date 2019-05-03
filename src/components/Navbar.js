16fd71531e855fec52d20e7248bb63ca8cc70aae
Joe Andaluz <jandaluz1@gmail.com>
2019-05-03 19:58:57 -0400

    moved navbar into its own file. bound onMapSelect to the state in App.js

---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+const newNavbar = props => (
+  <Navbar fixed="top" bg="dark" variant="dark">
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mr-auto">
+        <Nav.Link eventKey="erangel" onSelect={props.onMapSelect}>
+          Erangel
+        </Nav.Link>
+        <Nav.Link eventKey="miramar" onSelect={props.onMapSelect}>
+          Miramar
+        </Nav.Link>
+        <Nav.Link eventKey="sanhok" onSelect={props.onMapSelect}>
+          Sanhok
+        </Nav.Link>
+        <Nav.Link eventKey="vikendi" onSelect={props.onMapSelect}>
+          Vikendi
+        </Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+);
+
+export default newNavbar;
