de02f0ed07c25f399b4f4a08ebf0a4d38172c1dc
Joe Andaluz <jandaluz1@gmail.com>
2019-05-03 22:04:34 -0400

    fixed navbar being stuck, refactored HeatMap into functional component, mapInfo.js is used to change maps on selection

--- src/components/map/HeatMap.js
+++ src/components/map/HeatMap.js
@@ -1,127 +1,108 @@
-import React, { Component } from 'react';
-import * as d3 from "d3";
-import * as d3contours from "d3-contour";
+import React from 'react';
+import * as d3 from 'd3';
+import * as d3contours from 'd3-contour';
 
-class HeatMap extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      "mapName": props.mapName,
-      "mapUrl": props.mapUrl,
-      "rangeX": props.rangeX,
-      "rangeY": props.rangeY,
-      "domainX": props.domainX,
-      "domainY": props.domainY,
-    };
-  }
-  render = () => (
+const HeatMap = props => {
+  const { mapName, mapUrl, rangeX, rangeY, domainX, domainY } = props;
+
+  clearTheImage();
+
+  readTheCsv(mapName, mapUrl, rangeY, rangeX, domainX, domainY);
+
+  return (
     <div>
-      <div id="d3-svg"></div>
+      <div id="d3-svg" />
     </div>
-  )
+  );
+};
 
-  componentDidMount = () => {
-    console.log("componentDidMount");
-    this.loadTheMap();
-  }
-
-  componentDidUpdate = (prevProps, prevState, snapshot) => {
-    console.log("componentDidUpdate", this.props);
-    if (prevProps.mapName !== this.props.mapName &&
-      prevProps.mapUrl !== this.props.mapUrl) {
-      this.setState({
-        "mapName": this.props.mapName,
-        "mapUrl": this.props.mapUrl,
-        "domainX": this.props.domainX,
-        "domainY": this.props.domainY,
-        "rangeX": this.props.rangeX,
-        "rangeY" :this.props.rangeY
-      }, this.loadTheMap);
-      return true;
-    }
-  }
-  loadTheMap = () => {
-    this.clearTheImage();
-    if (this.state.mapName !== "") {
-      this.readTheCsv();
-    }
-  }
-
-  clearTheImage = () => {
-    var svg = d3.select("#d3-svg")
-    svg.selectAll("*").remove();
-  }
-  readTheCsv = () => {
-    console.log('read the csv');
-    let height = this.state.rangeY;
-    let width = this.state.rangeX;
-    var svg = d3.select("#d3-svg")
-      .append("svg")
-      .attr("width", width)
-      .attr("height", height)
-      .append("g")
-
-    let dataUrl = "/pubg-hackathon-published/landings/" + this.props.mapName + ".csv";
-    console.log(dataUrl);
-    d3.csv(dataUrl, {cache: "force-cache"}).then((data) => {
-      const landingCoords = data.filter((data) => {
-        return data.map_name === this.props.mapName;
-      }).map((data) => {
-        return {
-          "x": (+data.x) / 100,
-          "y": (+data.y) / 100
-        }
-      });
-      console.log(this.state.mapName);
-      console.log(landingCoords)
-
-      var xScale = d3.scaleLinear()
-        .domain([0, (+this.state.domainX)/100])
-        .range([0, width]);
-      var yScale = d3.scaleLinear()
-        .domain([0, (+this.state.domainY)/100])
-        .range([0, height])
-
-      if (landingCoords.length > 0) {
-        let contours = d3contours.contourDensity()
-          .x((d) => {
-            return xScale(d.x);
-          })
-          .y((d) => {
-            return yScale(d.y);
-          })
-          .size([this.state.domainX/100, this.state.domainY/100])
-          .cellSize(8)(landingCoords);
-
-        console.log(contours)
-        const lastContour = contours[contours.length - 1].value;
-        console.log(lastContour);
-        let maxDomain = lastContour;
-
-        // Prepare a color palette
-        var myColor = d3.scaleSequential()
-          .interpolator(d3.interpolateYlOrRd)
-          .domain([0, maxDomain])
-
-        d3.interpolateYlOrRd()
-
-
-        var myimage = svg.append('image')
-          .attr('xlink:href', this.state.mapUrl)
-          .attr('width', width)
-          .attr('height', height)
-        console.log("img");
-        console.log(myimage);
-
-        svg.insert("g")
-          .selectAll("path")
-          .data(contours)
-          .join("path")
-          .attr("d", d3.geoPath())
-          .attr("fill", function (d) { return myColor(d.value); })
-          .attr("class", "contour")
-      }
-    });
-  }
-}
 export default HeatMap;
+
+const clearTheImage = () => {
+  const svg = d3.select('#d3-svg');
+  svg.selectAll('*').remove();
+};
+
+const readTheCsv = (mapName, mapUrl, rangeY, rangeX, domainX, domainY) => {
+  console.log('read the csv');
+  let height = rangeY;
+  let width = rangeX;
+  const svg = d3
+    .select('#d3-svg')
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height)
+    .append('g');
+
+  let dataUrl = '/pubg-hackathon-published/landings/' + mapName + '.csv';
+  console.log(dataUrl);
+  d3.csv(dataUrl, { cache: 'force-cache' }).then(data => {
+    const landingCoords = data
+      .filter(data => {
+        return data.map_name === mapName;
+      })
+      .map(data => {
+        return {
+          x: +data.x / 100,
+          y: +data.y / 100
+        };
+      });
+    console.log(mapName);
+    console.log(landingCoords);
+
+    const xScale = d3
+      .scaleLinear()
+      .domain([0, +domainX / 100])
+      .range([0, width]);
+    const yScale = d3
+      .scaleLinear()
+      .domain([0, +domainY / 100])
+      .range([0, height]);
+
+    if (landingCoords.length > 0) {
+      let contours = d3contours
+        .contourDensity()
+        .x(d => {
+          return xScale(d.x);
+        })
+        .y(d => {
+          return yScale(d.y);
+        })
+        .size([domainX / 100, domainY / 100])
+        .cellSize(8)(landingCoords);
+
+      console.log(contours);
+      const lastContour = contours[contours.length - 1].value;
+      console.log(lastContour);
+      let maxDomain = lastContour;
+
+      // Prepare a color palette
+      const myColor = d3
+        .scaleSequential()
+        .interpolator(d3.interpolateYlOrRd)
+        .domain([0, maxDomain]);
+
+      d3.interpolateYlOrRd();
+
+      console.log('MAPURL', mapUrl);
+      const myimage = svg
+        .append('image')
+        .attr('xlink:href', mapUrl)
+        .attr('width', width)
+        .attr('height', height);
+      console.log('img');
+      console.log(myimage);
+
+      svg
+        .insert('g')
+        .selectAll('path')
+        .data(contours)
+        .join('path')
+        .attr('d', d3.geoPath())
+        .attr('fill', function(d) {
+          return myColor(d.value);
+        })
+        .attr('class', 'contour');
+    }
+  });
+};
---
+++ src/mapInfo.js
@@ -0,0 +1,35 @@
+const erangel = {
+  mapName: 'Erangel_Main',
+  mapUrl:
+    'https://raw.githubusercontent.com/pubg/api-assets/master/Assets/Maps/Erangel_Main_Low_Res.png',
+  domainX: 816000,
+  domainY: 816000
+};
+
+const miramar = {
+  mapName: 'Desert_Main',
+  mapUrl:
+    'https://raw.githubusercontent.com/pubg/api-assets//master/Assets/Maps/Miramar_Main_Low_Res.png',
+  domainX: 816000,
+  domainY: 816000
+};
+
+const sanhok = {
+  mapName: 'Savage_Main',
+  mapUrl:
+    'https://raw.githubusercontent.com/pubg/api-assets//master/Assets/Maps/Sanhok_Main_Low_Res.png',
+  domainX: 408000,
+  domainY: 408000
+};
+
+const vikendi = {
+  mapName: 'DihorOtok_Main',
+  mapUrl:
+    'https://raw.githubusercontent.com/pubg/api-assets/master/Assets/Maps/Vikendi_Main_Low_Res.png',
+  domainX: 612000,
+  domainY: 612000
+};
+
+const maps = { erangel, miramar, sanhok, vikendi };
+
+export default maps;
