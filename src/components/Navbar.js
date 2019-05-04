12a6986ae1195548f0492c14d08b70af9bb942db
jamison.andaluz@gmail.com <jamison.andaluz@gmail.com>
2019-05-03 22:19:22 -0400

    hover fade-in for navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,24 +3,26 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 
 const newNavbar = props => (
-  <Navbar fixed="top" bg="dark" variant="dark">
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="mr-auto">
-        <Nav.Link eventKey="erangel" onSelect={props.onMapSelect}>
-          Erangel
-        </Nav.Link>
-        <Nav.Link eventKey="miramar" onSelect={props.onMapSelect}>
-          Miramar
-        </Nav.Link>
-        <Nav.Link eventKey="sanhok" onSelect={props.onMapSelect}>
-          Sanhok
-        </Nav.Link>
-        <Nav.Link eventKey="vikendi" onSelect={props.onMapSelect}>
-          Vikendi
-        </Nav.Link>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
+  <div id="slide" className="nav-container">
+    <Navbar fixed="top" bg="dark" variant="dark">
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link eventKey="erangel" onSelect={props.onMapSelect}>
+            Erangel
+          </Nav.Link>
+          <Nav.Link eventKey="miramar" onSelect={props.onMapSelect}>
+            Miramar
+          </Nav.Link>
+          <Nav.Link eventKey="sanhok" onSelect={props.onMapSelect}>
+            Sanhok
+          </Nav.Link>
+          <Nav.Link eventKey="vikendi" onSelect={props.onMapSelect}>
+            Vikendi
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  </div>
 );
 
 export default newNavbar;
