09f72f9febff1db18b6343e236fee61173831f6a
jamison.andaluz@gmail.com <jamison.andaluz@gmail.com>
2019-05-14 00:17:36 -0400

    settings updates

--- src/windows/settings/Settings.js
+++ src/windows/settings/Settings.js
@@ -61,30 +61,18 @@ class Settings extends Component {
   render() {
     return (
       <div className="settings">
-				<svg xmlns='http://www.w3.org/2000/svg' display='none'>
-   				 <symbol id='window-control_close' viewBox='0 0 30 30'>
-       			 <line x1='19.5' y1='10.5' x2='10.5' y2='19.5' fill='none' stroke='currentcolor'
-							strokeLinecap='round' />
-							<line x1='10.5' y1='10.5' x2='19.5' y2='19.5' fill='none' stroke='currentcolor'
-      				  strokeLinecap='round' />
-    			</symbol>
-				</svg>
-
-				<header className="app-header" ref={this._headerRef}>
-					<div className="window-controls-group">
-						<button className="icon window-control window-control-close" id="closeButton" onClick={this.onCloseClicked}>
-							<svg>
-								<use xlinkHref="#window-control_close" />
-							</svg>
-						</button>
-					</div>
-				</header>
-
-				<main>
-					<div><span>Toggle:&nbsp;</span><kbd id="toggle">{this.state.toggleText}</kbd></div>
-					<div><span>Show heatmaps&nbsp;</span><kbd id="heatmap">{this.state.heatmapText}</kbd></div>
-				</main>
-
+	  	<h1>Welcome to PUBG: Hotdrops!</h1>
+		<p>Squad can't decide where to drop? Find out where the hot zones are for each map and jump into the action</p>
+		<section>
+			<h3>How do I show/hide the Hotdrop heat map?</h3>
+			<p><span>You can toggle the heat map on and off using</span><kbd id="heatmap">{this.state.heatmapText}</kbd></p>
+			<h3>How do I select a map?</h3>
+			<p>It's easy! Hover your mouse over the top portion of the heat map to bring the map selection bar to the foreground</p>
+			<h3>When does this Hotdrop heat map show? I don't want it in the way while I'm in game</h3>
+			<p>The heat map is currently designed to only be in the foreground in the lobby; it should minimize itself once you enter the loading phase</p>
+			<h3>I don't want this here, I want it over there! How do I drag the Hotdrop heat map?</h3>
+			<p>Hover the mouse over the top portion of the map to illuminate the navbar; click in an empty space in the toolbar and drag it wherever you want</p>
+		</section>
       </div>
     );
   }
